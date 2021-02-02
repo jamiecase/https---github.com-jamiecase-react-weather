4f7634aa89cf112506d724dbaaaa168d0142ddfd
Jamie Case <70907004+jamiecase@users.noreply.github.com>
2021-02-01 21:33:15 -0800

    formatteddata

--- src/FormattedData.js
+++ src/FormattedData.js
@@ -1,27 +1,43 @@
 import React from "react";
 
 export default function FormattedData(props) {
-    let days=[
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
-    let day = days[props.data.getDay()];
-    let hours = props.data.getHours();
-    if (hours <10) {
-        hours = `0${minutes}`;
-    }
-    let minutes = props.data.getMinutes();
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    return (
-        <div>
-            {day}  {hours}:{minutes}
-        </div>
-    );
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[props.date.getDay()];
+  let today = props.date.getDate();
+  let months = [
+    "Janurary",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[props.date.getMonth()];
+
+  let hours = props.date.getHours().toString().padStart(2, "0");
+
+  let minutes = props.date.getMinutes().toString().padStart(2, "0");
+
+  return (
+    <h3>
+      Last updated:{" "}
+      <span className="date-time">
+        {day}, {today} {month} | {hours}:{minutes}
+      </span>
+    </h3>
+  );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,99 +1,92 @@
-
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import FormattedData from "./FormattedData";
-
 export default function Weather() {
-  const [weatherdata, setWeatherData] = useState ({ ready: false }) ;
-
+  const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
-      ready:true,
+      ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: "wednesday 07:00",
-      description:response.data.weather[0].description,
+      description: response.data.weather[0].description,
       imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
-      wind:response.data.wind.speed,
-      city:response.data.name,
+      wind: response.data.wind.speed,
+      city: response.data.name,
     });
-    if(weatherData.ready){
-     return (
-    <div className="App">
-      <div className="Weather">
-        <form className="mt-2 mb-3">
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Type a city name"
-                className="form-control"
-                autofocus="on"
-                autocomplete="off"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-outline-secondary w-100"
-              />
-            </div>
-          </div>
-        </form>
-        <div className="weather-overview">
-          <h1>{weatherData.city}</h1>
-          <ul>
-            <li>
-              <i>
-                <FormattedDate date= {weatherData.date}/>
-              </i>
-            </li>
-            <li>{weatherData.description}</li>
-          </ul>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <div className="clearfix current-temperature">
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <strong>{weatherData.temperature}</strong>
-                <span className="units">
-                  <a href="/">°C</a> | <a href="/">°F</a>
-                </span>
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="Weather">
+          <form className="mt-2 mb-3">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city name"
+                  className="form-control"
+                  autofocus="on"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-outline-secondary w-100"
+                />
               </div>
             </div>
-          </div>
-
-          <div className="col-5 humidity-and-wind">
+          </form>
+          <div className="weather-overview">
+            <h1>{weatherData.city}</h1>
             <ul>
               <li>
-                Humidity: <span>{weatherData.humidity}</span>%
+                <i>
+                  <FormattedDate date={weatherData.date} />
+                </i>
               </li>
-              <li className="wind-li">
-                Wind: <span>{weatherData.wind}</span> km/h
-              </li>
+              <li>{weatherData.description}</li>
             </ul>
           </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix current-temperature">
+                <img
+                  src={weatherData.imgUrl}
+                  alt={weatherData.description}
+                  className="float-left"
+                />
+                <div className="float-left">
+                  <strong>{weatherData.temperature}</strong>
+                  <span className="units">
+                    <a href="/">°C</a> | <a href="/">°F</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-5 humidity-and-wind">
+              <ul>
+                <li>
+                  Humidity: <span>{weatherData.humidity}</span>%
+                </li>
+                <li className="wind-li">
+                  Wind: <span>{weatherData.wind}</span> km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="row"></div>
         </div>
-        <div className="row"></div>
       </div>
-      );
-       } else {
-        const apiKey ="0e7fda8f04980882d2b3cbcbbbb1c0e4";
-        let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-        axios.get(apiURL).then(handleResponse);
-        return"Loading...";
-      }
-      
-    
-      
-    </div>
-  ); } 
-
+    );
+  } else {
+    const apiKey = "0e7fda8f04980882d2b3cbcbbbb1c0e4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
+}
