c55ec8d87956b3851c411f02a7db29b1f5c877a7
Jamie Case <70907004+jamiecase@users.noreply.github.com>
2021-02-02 16:11:35 -0800

    added props

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import FormattedData from "./FormattedData";
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
@@ -85,7 +85,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "0e7fda8f04980882d2b3cbcbbbb1c0e4";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
   }
